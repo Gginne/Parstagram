dfe1ade83a81cd8c15c41bfc5c85aa3ab407c178
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-03-02 14:12:34 -0500

    Added connected login to parse user class

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.parstagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,29 +9,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
+    EditText etUsername;
+    EditText etPassword;
+    Button loginButton;
+    ProgressBar loadingProgressBar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etUsername = findViewById(R.id.username);
-        final EditText etPassword = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        loadingProgressBar = findViewById(R.id.loading);
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
+
                 Log.i(TAG, "OnClick Login button");
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
@@ -41,5 +50,23 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "Attempting to login user: "+ username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with login", e);
+                } else {
+                    loadingProgressBar.setVisibility(View.VISIBLE);
+                    goToMainActivity();
+                    Toast.makeText(LoginActivity.this, "SUCCESS!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void goToMainActivity(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
