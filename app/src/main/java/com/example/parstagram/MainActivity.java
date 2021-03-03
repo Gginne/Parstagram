dee6e0966465054ed54d8f5a06b9a4899d694b9c
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-03-02 19:08:33 -0500

    User can query posts to parse

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -20,5 +31,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.description);
+        takePicture = findViewById(R.id.takePicture);
+        savePicture = findViewById(R.id.savePicture);
+        ivPicture = findViewById(R.id.picture);
+
+        //queryPosts();
+
+        savePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                if(description.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Description cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description, currentUser);
+            }
+        });
+    }
+
+    private void savePost(String description, ParseUser currentUser) {
+        Post post = new Post();
+        post.setDescription(description);
+        //post.setImage(image);
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(MainActivity.this, "Error while saving", Toast.LENGTH_SHORT).show();
+                }
+                Log.i(TAG, "Post saved successfuly");
+                etDescription.setText("");
+            }
+        });
+    }
+
+    private void queryPosts(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Couldn't fetch posts", e);
+                }
+
+                for(Post post: posts){
+                    Log.i(TAG, "Post "+ post.getDescription());
+                }
+            }
+        });
     }
 }
